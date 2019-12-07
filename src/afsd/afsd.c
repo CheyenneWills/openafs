dfdb68ffaee8583034e4c5ed3ff256b73029852d
Cheyenne Wills <cwills@sinenomine.net>
2019-12-06 17:42:47 -0700

    WIP: Use hostSA instead of HostAddr

--- src/afsd/afsd.c
+++ src/afsd/afsd.c
@@ -143,6 +143,7 @@
 #include <ctype.h>
 
 #include <afs/afs_args.h>
+#include <rx/rx.h>
 #include <afs/cellconfig.h>
 #include <afs/afssyscalls.h>
 #include <afs/afsutil.h>
@@ -1439,7 +1440,7 @@ ConfigCell(struct afsconf_cell *aci, void *arock, struct afsconf_dir *adir)
     }
     /* build address list */
     for (i = 0; i < MAXHOSTSPERCELL; i++)
-	memcpy(&hosts[i], &aci->hostAddr[i].sin_addr, sizeof(afs_int32));
+	memcpy(&hosts[i], &aci->hostSA[i].u.in,sin_addr, sizeof(afs_int32));
 
     if (aci->linkedCell)
 	cellFlags |= 4;		/* Flag that linkedCell arg exists,
@@ -1511,7 +1512,7 @@ AfsdbLookupHandler(void)
 	    else
 		kernelMsg[1] = 0;
 	    for (i = 0; i < acellInfo.numServers; i++)
-		kernelMsg[i + 2] = acellInfo.hostAddr[i].sin_addr.s_addr;
+		kernelMsg[i + 2] = acellInfo.hostSA[i].u.in.sin_addr.s_addr;
 	    strncpy(acellName, acellInfo.name, sizeof(acellName));
 	    acellName[sizeof(acellName) - 1] = '\0';
 	}
